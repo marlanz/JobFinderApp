b9c0bf16d39b0ee8a6aa0591a84e31f7e05c250c
phanh <anhddpse172448@fpt.edu.vn>
2024-04-09 19:17:59 +0700

    Job Finder Mobile App

---
+++ app/index.js
@@ -0,0 +1,54 @@
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
+import { Stack, useRouter } from "expo-router";
+import { COLORS, icons, images, SIZES, SHADOWS } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+export default function Home() {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitleAlign: "center",
+          headerTitle: "",
+        }}
+      />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View
+          style={{
+            flex: 1,
+            padding: SIZES.medium,
+          }}
+        >
+          <Welcome
+            searchTerm={searchTerm}
+            setSearchTerm={setSearchTerm}
+            handleClick={() => {
+              if (searchTerm) {
+                router.push(`/search/${searchTerm}`);
+              }
+            }}
+          />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
